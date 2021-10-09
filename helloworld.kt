18c2711d2ba9b13a5e9145ac4eb570628a70dfc6
Kirill Bondarev <Gut54566@yandex.ru>
2021-10-09 22:12:31 +0700

    Function square added.
    Function that is able to square a given number.

--- helloworld.kt
+++ helloworld.kt
@@ -2,6 +2,8 @@ package gitLearningRepo
 
 fun sum(a: Int, b: Int): Int = a + b
 
+fun square(a: Double): Double = a*a
+
 fun main(){
 	println("hello world")
 	println("Sum of 3 and 4 is equal to $sum(a = 3, b = 4))
